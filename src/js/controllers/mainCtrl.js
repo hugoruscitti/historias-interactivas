a931868e5923079769864c99caa96c92538e0bb6
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-09-29 15:24:21 -0300

    Agregando bootstrap y arreglos generales.

--- src/js/controllers/mainCtrl.js
+++ src/js/controllers/mainCtrl.js
@@ -4,28 +4,30 @@ app.controller('MainCtrl', function($scope, Escenas) {
   $scope.data = {};
   $scope.data.mouse_x = 0;
   $scope.data.mouse_y = 0;
-  $scope.data.depuracion = false;
   $scope.data.areas_visibles = true;
-  //$scope.data.escenas = Escenas.obtener_escenas();
   $scope.data.nombres_escenas = Escenas.obtener_nombres_escenas();
-  //$scope.data.escena_actual = 'main';
   $scope.data.destino = '';
   $scope.data.item_seleccionado_id = -1;
   $scope.data.item_seleccionado = {};
+  $scope.data.preferencias = Escenas.obtener_preferencias_del_proyecto();
 
-
-  $scope.data.escena_actual = Escenas.definir_escena('main');
+  $scope.zoom = function() {
+      /* Realiza un acercamiento a la ventana principal de juego. */
+      zoom.to({
+          element: document.querySelector('.canvas')
+      });
+  };
 
   $scope.guardar = function() {
     Escenas.guardar();
-  }
+  };
 
   $scope.restaurar = function() {
     Escenas.restaurar(function() {
       $scope.data.escena_actual = Escenas.recargar_escena();
       $scope.$apply();
     });
-  }
+  };
 
   /**
    * Cambia la escena escena actual.
@@ -33,11 +35,11 @@ app.controller('MainCtrl', function($scope, Escenas) {
   $scope.ir_a = function(nombre_escena) {
     $scope.deseleccionar_areas();
     $scope.data.escena_actual = Escenas.definir_escena(nombre_escena);
-  }
+  };
 
   $scope.crear_area = function() {
     Escenas.crear_area();
-  }
+  };
 
   /**
    * Captura la posición del mouse.
@@ -45,7 +47,7 @@ app.controller('MainCtrl', function($scope, Escenas) {
   $scope.on_move = function(ev) {
     $scope.data.mouse_x = ev.x;
     $scope.data.mouse_y = ev.y;
-  }
+  };
 
   /**
    * Se ejecuta por parte de los objetos area cuando el mouse pasa
@@ -56,26 +58,23 @@ app.controller('MainCtrl', function($scope, Escenas) {
       $scope.data.destino = "";
     else
       $scope.data.destino = n.nombre;
-  }
-
-  $scope.obtener_estilo_item_lista = function(n) {
-    if (n.id === $scope.data.item_seleccionado_id)
-      return {'background-color': 'yellow'};
-    else
-      return {};
-  }
+  };
 
   /* Selecciona un area para editarla. */
   $scope.seleccionar = function(n) {
     $scope.data.item_seleccionado_id = n.id;
     $scope.data.item_seleccionado = n;
-  }
+  };
 
   /* Deselecciona todas las areas de modo que se oculte la edición de area. */
   $scope.deseleccionar_areas = function() {
     $scope.data.item_seleccionado_id = -1;
     $scope.data.item_seleccionado = {};
-  }
+  };
+
+  $scope.eliminar_area = function(n) {
+      Escenas.eliminar_area(n);
+  };
 
   $scope.obtener_estilo_area = function(n) {
     var tmp = {left: n.x, top: n.y, width: n.w, height: n.h};
@@ -86,9 +85,16 @@ app.controller('MainCtrl', function($scope, Escenas) {
       tmp.opacity = 0;
 
     if ($scope.data.item_seleccionado_id === n.id)
-      tmp.backgroundColor = "red";
+      tmp.backgroundColor = "fff99a";
 
     return tmp;
-  }
+  };
 
+  $scope.obtener_estilo_canvas = function() {
+    return Escenas.obtener_estilo_canvas();
+  };
+
+
+  $scope.data.escena_actual = Escenas.definir_escena('main');
+  $scope.restaurar();
 });
--- src/js/factories/escenasFactory.js
+++ src/js/factories/escenasFactory.js
@@ -5,50 +5,7 @@ var fs = require('fs');
 app.factory('Escenas', function() {
   var obj = {};
   var nombre_de_la_escena_actual = '';
-  var data = {
-    main: {
-      fondo: 'casa.png',
-      puertas: [
-      {
-        id: 1233333,
-        x: 600,
-        y: 210,
-        w: 70,
-        h: 120,
-        nombre: 'puerta a la habitación',
-        destino: 'cuarto'
-      },
-      {
-        id: 344444,
-        x: 100,
-        y: 110,
-        w: 20,
-        h: 20,
-        nombre: 'bug',
-        destino: 'cuarto'
-      }
-      ],
-    },
-    cuarto: {
-      fondo: 'cuarto.png',
-      puertas: [
-      {
-        id: 1733,
-        x: 256,
-        y: 256,
-        w: 70,
-        h: 120,
-        nombre: 'salir',
-        destino: 'main'
-      }
-      ],
-    },
-    calle: {
-      fondo: 'calle.png',
-      puertas: [
-      ]
-    },
-  }
+  var data = {};
 
   obj.obtener_nombres_escenas = function() {
     return [
@@ -56,7 +13,7 @@ app.factory('Escenas', function() {
       "cuarto",
       "calle",
     ]
-  }
+  };
 
   obj.crear_area = function() {
     var escena_actual = obj.obtener_escena_actual();
@@ -69,27 +26,35 @@ app.factory('Escenas', function() {
       h: 190,
       nombre: '...',
       destino: ''
-    }
+    };
 
     escena_actual.puertas.push(area);
-  }
+  };
+
+  obj.eliminar_area = function(area) {
+    var lista = obj.obtener_escena_actual().puertas;
+
+    for (var i in lista) {
+        if (lista[i].id === area.id) {
+            lista.splice(i, 1)
+        }
+    }
+  };
 
   obj.restaurar = function(success) {
     var filename = './escenas.json';
 
     fs.readFile(filename, 'utf8', function (err, jsondata) {
       if (err) {
-        alert(err)
+        alert(err);
         return;
       }
 
       data = JSON.parse(jsondata);
-      console.log("cargando escena");
 
       success.call();
-
     });
-  }
+  };
 
   obj.guardar = function() {
     var filename = './escenas.json';
@@ -101,7 +66,7 @@ app.factory('Escenas', function() {
         console.log("JSON saved to " + filename);
       }
     });
-  }
+  };
 
   obj.obtener_escenas = function() {
     return data;
@@ -110,18 +75,33 @@ app.factory('Escenas', function() {
   obj.definir_escena = function(nombre_de_la_escena) {
     nombre_de_la_escena_actual = nombre_de_la_escena;
     return obj.obtener_escena_actual();
-  }
+  };
 
   obj.recargar_escena = function() {
     return obj.definir_escena(nombre_de_la_escena_actual);
-  }
+  };
 
   /*
    * Retorna el objeto que representa la escena actual completa.
    */
   obj.obtener_escena_actual = function() {
     return obj.obtener_escenas()[nombre_de_la_escena_actual];
-  }
+  };
+
+  obj.obtener_estilo_canvas = function() {
+    var proyecto = obj.obtener_preferencias_del_proyecto();
+    var tmp = {};
+
+    tmp.width = proyecto.ancho || 0;
+    tmp.height = proyecto.alto || 0;
+
+    return tmp;
+  };
+
+  obj.obtener_preferencias_del_proyecto = function() {
+      console.log(data);
+      return data._proyecto || {};
+  };
 
   return obj;
-})
+});
